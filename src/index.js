16bf54f96e5885625151611e0652e5a6de1ec838
Abdullah Zuberi <111603644+Abdullah-Zuberi@users.noreply.github.com>
2023-02-21 11:50:43 +0500

    Simple greeting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM  from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let currDate = new Date(2020,5,5,24);
+currDate = currDate.getHours();
+let greeting = '';
+const cssStyle = {};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if(currDate >= 1 && currDate <12){
+   greeting= 'Good Morning';
+   cssStyle.color = 'green';
+}else if(currDate>=12 && currDate < 19){
+  greeting= "Good Afternoon";
+  cssStyle.color = 'Orange';
+}else{
+  greeting= "Good Night";
+  cssStyle.color = 'Black';
+}
+ 
+ReactDOM.render(
+  <>
+  <div>
+  <h1>Hello Sir,<span style={cssStyle}>{greeting}</span></h1>,
+  </div>
+  </>,
+   document.getElementById("root")
+);  
+
+
+
+// import React from "react";
+// import ReactDOM  from "react-dom";
+
+// const name="Abdullah";
+
+// ReactDOM.render(
+
+//   <>
+//   <h1 contentEditable="true">My Name is {name}</h1>
+//   <img src="https://picsum.photos/200/300" />
+//   </>,
+//   document.getElementById("root")
+// );
+
+
+
+// import React from "react";
+// import ReactDOM  from "react-dom";
+
+// ReactDOM.render(
+//  <>
+ 
+//   <h1>Netflix Series Picker</h1>,
+//   <p>Here are the list of my fav 5 Netflix Series</p>
+//   <ol>
+//   <li> DARK </li>
+//   <li> Extra curricular </li>
+//   <li> My Holo love </li>
+//   <li> My First-2 love </li>
+//   <li> Mr Robot </li>
+//   </ol>
+
+//   </>,
+
+//   document.getElementById("root") 
+// );
+
+
+
+// import React from "react";
+// import ReactDOM  from "react-dom";
+
+// const name="Abdullah";
+// const currDate = new Date().toLocaleDateString();
+// const currTime= new Date().toLocaleTimeString();
+
+// ReactDOM.render(
+// <>
+// <h1>Hello,My Name is {name} </h1>
+// <p>Current Date is= {currDate} </p>
+// <p>Current Time is= {currTime} </p>
+// </>,
+// document.getElementById("root")
+// );
